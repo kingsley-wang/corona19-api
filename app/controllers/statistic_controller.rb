cab62aa3d973b9c79b1f70ef8eead470846cf3b0
Kingsley <kingsley.wang@activepipe.com>
2020-03-17 10:30:15 +1100

    Add ACT

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -10,7 +10,8 @@ class StatisticController < ApplicationController
       @wa.state_records.pluck(:confirmed)[n] +
       @sa.state_records.pluck(:confirmed)[n] +
       @tas.state_records.pluck(:confirmed)[n] +
-      @nt.state_records.pluck(:confirmed)[n])
+      @nt.state_records.pluck(:confirmed)[n]) +
+      @act.state_records.pluck(:confirmed)[n])
     end
 
     @trending = {
@@ -22,6 +23,7 @@ class StatisticController < ApplicationController
         'Western Australia': @wa.state_records.pluck(:confirmed),
         'South Australia': @sa.state_records.pluck(:confirmed),
         'Tasmania': @tas.state_records.pluck(:confirmed),
+        'Australian Capital Territory': @act.state_records.pluck(:confirmed),
         'Northern Territory': @nt.state_records.pluck(:confirmed),
       },
       "All Australia": total
@@ -37,6 +39,7 @@ class StatisticController < ApplicationController
                             @wa.state_records[-1].confirmed +
                             @sa.state_records[-1].confirmed +
                             @tas.state_records[-1].confirmed +
+                            @act.state_records[-1].confirmed +
                             @nt.state_records[-1].confirmed
 
     today_total_recovered = @nsw.state_records[-1].recovered.to_i +
@@ -45,6 +48,7 @@ class StatisticController < ApplicationController
                             @wa.state_records[-1].recovered.to_i +
                             @sa.state_records[-1].recovered.to_i +
                             @tas.state_records[-1].recovered.to_i +
+                            @act.state_records[-1].recovered.to_i +
                             @nt.state_records[-1].recovered.to_i
 
     today_total_deaths = @nsw.state_records[-1].deaths.to_i +
@@ -53,6 +57,7 @@ class StatisticController < ApplicationController
                          @wa.state_records[-1].deaths.to_i +
                          @sa.state_records[-1].deaths.to_i +
                          @tas.state_records[-1].deaths.to_i +
+                         @act.state_records[-1].deaths.to_i +
                          @nt.state_records[-1].deaths.to_i
 
     lastday_total_confirmed = @nsw.state_records[-2].confirmed +
@@ -61,6 +66,7 @@ class StatisticController < ApplicationController
                               @wa.state_records[-2].confirmed +
                               @sa.state_records[-2].confirmed +
                               @tas.state_records[-2].confirmed +
+                              @act.state_records[-2].confirmed +
                               @nt.state_records[-2].confirmed
 
     lastday_total_recovered = @nsw.state_records[-2].recovered.to_i +
@@ -69,6 +75,7 @@ class StatisticController < ApplicationController
                               @wa.state_records[-2].recovered.to_i +
                               @sa.state_records[-2].recovered.to_i +
                               @tas.state_records[-2].recovered.to_i +
+                              @act.state_records[-2].recovered.to_i +
                               @nt.state_records[-2].recovered.to_i
 
     lastday_total_deaths = @nsw.state_records[-2].deaths.to_i +
@@ -77,30 +84,16 @@ class StatisticController < ApplicationController
                            @wa.state_records[-2].deaths.to_i +
                            @sa.state_records[-2].deaths.to_i +
                            @tas.state_records[-2].deaths.to_i +
+                           @act.state_records[-2].deaths.to_i +
                            @nt.state_records[-2].deaths.to_i
-
-    total_recovered = @nsw.state_records.sum(:recovered) +
-                      @vic.state_records.sum(:recovered) +
-                      @qld.state_records.sum(:recovered) +
-                      @wa.state_records.sum(:recovered) +
-                      @sa.state_records.sum(:recovered) +
-                      @tas.state_records.sum(:recovered) +
-                      @nt.state_records.sum(:recovered)
-    total_deaths = @nsw.state_records.sum(:deaths) +
-                    @vic.state_records.sum(:deaths) +
-                    @qld.state_records.sum(:deaths) +
-                    @wa.state_records.sum(:deaths) +
-                    @sa.state_records.sum(:deaths) +
-                    @tas.state_records.sum(:deaths) +
-                    @nt.state_records.sum(:deaths)
 
      @overview = {
       "Time": Time.now.strftime("%m-%d-%Y %H:%M%P"),
       "Confirmed": today_total_confirmed,
       "Confirmed_new": today_total_confirmed - lastday_total_confirmed,
-      "Recovered": total_recovered,
+      "Recovered": today_total_recovered,
       "Recovered_new": today_total_recovered - lastday_total_recovered,
-      "Deaths": total_deaths,
+      "Deaths": today_total_deaths,
       "Deaths_new": today_total_deaths - lastday_total_deaths,
      }
 
@@ -117,5 +110,6 @@ class StatisticController < ApplicationController
     @sa = State.find_by_name('South Australia')
     @tas = State.find_by_name('Tasmania')
     @nt = State.find_by_name('Northern Territory')
+    @act = State.find_by_name('Australian Capital Territory')
   end
 end
