5b572ad5e5d00b537f60ef6c3f5a789e54122ef2
Kingsley <kingsley.wang@activepipe.com>
2020-03-16 17:22:35 +1100

    Add state field

--- app/admin/state_records.rb
+++ app/admin/state_records.rb
@@ -14,5 +14,24 @@ ActiveAdmin.register StateRecord do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  form do |f|
+    f.inputs do
+      f.input :state, as: :select
+      f.input :confirmed
+      f.input :deaths
+      f.input :recovered
+      f.input :created_at, as: :datepicker,
+        datepicker_options: {
+          min_date: "2020-1-1",
+          max_date: "+3D"
+        }
+      f.input :updated_at, as: :datepicker,
+        datepicker_options: {
+          min_date: "2020-1-1",
+          max_date: "+3D"
+        }
+    end
+    f.actions
+  end
   
 end
