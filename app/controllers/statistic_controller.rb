b96d10b57b9b4eaaaab424d835fee131f403817b
Kingsley <kingsley.wang@activepipe.com>
2020-03-24 10:54:13 +1100

    Add order

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -4,14 +4,14 @@ class StatisticController < ApplicationController
   def trending
     total = []
     @nsw.state_records.count.times do |n|
-      total.push(@nsw.state_records.pluck(:confirmed)[n] +
-      @vic.state_records.pluck(:confirmed)[n] +
-      @qld.state_records.pluck(:confirmed)[n] +
-      @wa.state_records.pluck(:confirmed)[n] +
-      @sa.state_records.pluck(:confirmed)[n] +
-      @tas.state_records.pluck(:confirmed)[n] +
-      @nt.state_records.pluck(:confirmed)[n]+
-      @act.state_records.pluck(:confirmed)[n])
+      total.push(@nsw.state_records.order('created_at ASC').pluck(:confirmed)[n] +
+      @vic.state_records.order('created_at ASC').pluck(:confirmed)[n] +
+      @qld.state_records.order('created_at ASC').pluck(:confirmed)[n] +
+      @wa.state_records.order('created_at ASC').pluck(:confirmed)[n] +
+      @sa.state_records.order('created_at ASC').pluck(:confirmed)[n] +
+      @tas.state_records.order('created_at ASC').pluck(:confirmed)[n] +
+      @nt.state_records.order('created_at ASC').pluck(:confirmed)[n]+
+      @act.state_records.order('created_at ASC').pluck(:confirmed)[n])
     end
 
     @trending = {
@@ -33,59 +33,59 @@ class StatisticController < ApplicationController
   end
 
   def overview
-    today_total_confirmed = @nsw.state_records[-1].confirmed +
-                            @vic.state_records[-1].confirmed +
-                            @qld.state_records[-1].confirmed +
-                            @wa.state_records[-1].confirmed +
-                            @sa.state_records[-1].confirmed +
-                            @tas.state_records[-1].confirmed +
-                            @act.state_records[-1].confirmed +
-                            @nt.state_records[-1].confirmed
+    today_total_confirmed = @nsw.state_records.order('created_at ASC')[-1].confirmed +
+                            @vic.state_records.order('created_at ASC')[-1].confirmed +
+                            @qld.state_records.order('created_at ASC')[-1].confirmed +
+                            @wa.state_records.order('created_at ASC')[-1].confirmed +
+                            @sa.state_records.order('created_at ASC')[-1].confirmed +
+                            @tas.state_records.order('created_at ASC')[-1].confirmed +
+                            @act.state_records.order('created_at ASC')[-1].confirmed +
+                            @nt.state_records.order('created_at ASC')[-1].confirmed
 
-    today_total_recovered = @nsw.state_records[-1].recovered.to_i +
-                            @vic.state_records[-1].recovered.to_i +
-                            @qld.state_records[-1].recovered.to_i +
-                            @wa.state_records[-1].recovered.to_i +
-                            @sa.state_records[-1].recovered.to_i +
-                            @tas.state_records[-1].recovered.to_i +
-                            @act.state_records[-1].recovered.to_i +
-                            @nt.state_records[-1].recovered.to_i
+    today_total_recovered = @nsw.state_records.order('created_at ASC')[-1].recovered.to_i +
+                            @vic.state_records.order('created_at ASC')[-1].recovered.to_i +
+                            @qld.state_records.order('created_at ASC')[-1].recovered.to_i +
+                            @wa.state_records.order('created_at ASC')[-1].recovered.to_i +
+                            @sa.state_records.order('created_at ASC')[-1].recovered.to_i +
+                            @tas.state_records.order('created_at ASC')[-1].recovered.to_i +
+                            @act.state_records.order('created_at ASC')[-1].recovered.to_i +
+                            @nt.state_records.order('created_at ASC')[-1].recovered.to_i
 
-    today_total_deaths = @nsw.state_records[-1].deaths.to_i +
-                         @vic.state_records[-1].deaths.to_i +
-                         @qld.state_records[-1].deaths.to_i +
-                         @wa.state_records[-1].deaths.to_i +
-                         @sa.state_records[-1].deaths.to_i +
-                         @tas.state_records[-1].deaths.to_i +
-                         @act.state_records[-1].deaths.to_i +
-                         @nt.state_records[-1].deaths.to_i
+    today_total_deaths = @nsw.state_records.order('created_at ASC')[-1].deaths.to_i +
+                         @vic.state_records.order('created_at ASC')[-1].deaths.to_i +
+                         @qld.state_records.order('created_at ASC')[-1].deaths.to_i +
+                         @wa.state_records.order('created_at ASC')[-1].deaths.to_i +
+                         @sa.state_records.order('created_at ASC')[-1].deaths.to_i +
+                         @tas.state_records.order('created_at ASC')[-1].deaths.to_i +
+                         @act.state_records.order('created_at ASC')[-1].deaths.to_i +
+                         @nt.state_records.order('created_at ASC')[-1].deaths.to_i
 
-    lastday_total_confirmed = @nsw.state_records[-2].confirmed +
-                              @vic.state_records[-2].confirmed +
-                              @qld.state_records[-2].confirmed +
-                              @wa.state_records[-2].confirmed +
-                              @sa.state_records[-2].confirmed +
-                              @tas.state_records[-2].confirmed +
-                              @act.state_records[-2].confirmed +
-                              @nt.state_records[-2].confirmed
+    lastday_total_confirmed = @nsw.state_records.order('created_at ASC')[-2].confirmed +
+                              @vic.state_records.order('created_at ASC')[-2].confirmed +
+                              @qld.state_records.order('created_at ASC')[-2].confirmed +
+                              @wa.state_records.order('created_at ASC')[-2].confirmed +
+                              @sa.state_records.order('created_at ASC')[-2].confirmed +
+                              @tas.state_records.order('created_at ASC')[-2].confirmed +
+                              @act.state_records.order('created_at ASC')[-2].confirmed +
+                              @nt.state_records.order('created_at ASC')[-2].confirmed
 
-    lastday_total_recovered = @nsw.state_records[-2].recovered.to_i +
-                              @vic.state_records[-2].recovered.to_i +
-                              @qld.state_records[-2].recovered.to_i +
-                              @wa.state_records[-2].recovered.to_i +
-                              @sa.state_records[-2].recovered.to_i +
-                              @tas.state_records[-2].recovered.to_i +
-                              @act.state_records[-2].recovered.to_i +
-                              @nt.state_records[-2].recovered.to_i
+    lastday_total_recovered = @nsw.state_records.order('created_at ASC')[-2].recovered.to_i +
+                              @vic.state_records.order('created_at ASC')[-2].recovered.to_i +
+                              @qld.state_records.order('created_at ASC')[-2].recovered.to_i +
+                              @wa.state_records.order('created_at ASC')[-2].recovered.to_i +
+                              @sa.state_records.order('created_at ASC')[-2].recovered.to_i +
+                              @tas.state_records.order('created_at ASC')[-2].recovered.to_i +
+                              @act.state_records.order('created_at ASC')[-2].recovered.to_i +
+                              @nt.state_records.order('created_at ASC')[-2].recovered.to_i
 
-    lastday_total_deaths = @nsw.state_records[-2].deaths.to_i +
-                           @vic.state_records[-2].deaths.to_i +
-                           @qld.state_records[-2].deaths.to_i +
-                           @wa.state_records[-2].deaths.to_i +
-                           @sa.state_records[-2].deaths.to_i +
-                           @tas.state_records[-2].deaths.to_i +
-                           @act.state_records[-2].deaths.to_i +
-                           @nt.state_records[-2].deaths.to_i
+    lastday_total_deaths = @nsw.state_records.order('created_at ASC')[-2].deaths.to_i +
+                           @vic.state_records.order('created_at ASC')[-2].deaths.to_i +
+                           @qld.state_records.order('created_at ASC')[-2].deaths.to_i +
+                           @wa.state_records.order('created_at ASC')[-2].deaths.to_i +
+                           @sa.state_records.order('created_at ASC')[-2].deaths.to_i +
+                           @tas.state_records.order('created_at ASC')[-2].deaths.to_i +
+                           @act.state_records.order('created_at ASC')[-2].deaths.to_i +
+                           @nt.state_records.order('created_at ASC')[-2].deaths.to_i
 
      @overview = {
       "Time": Time.now.strftime("%m-%d-%Y %H:%M%P"),
@@ -102,14 +102,14 @@ class StatisticController < ApplicationController
 
   def live_states_confirmed
     @overview = {
-      'New South Wales': @nsw.state_records[-1].confirmed,
-      'Victoria': @vic.state_records[-1].confirmed,
-      'Queensland': @qld.state_records[-1].confirmed,
-      'Western Australia': @wa.state_records[-1].confirmed,
-      'South Australia': @sa.state_records[-1].confirmed,
-      'Tasmania': @tas.state_records[-1].confirmed,
-      'Australian Capital Territory': @act.state_records[-1].confirmed,
-      'Northern Territory': @nt.state_records[-1].confirmed,
+      'New South Wales': @nsw.state_records.order('created_at ASC')[-1].confirmed,
+      'Victoria': @vic.state_records.order('created_at ASC')[-1].confirmed,
+      'Queensland': @qld.state_records.order('created_at ASC')[-1].confirmed,
+      'Western Australia': @wa.state_records.order('created_at ASC')[-1].confirmed,
+      'South Australia': @sa.state_records.order('created_at ASC')[-1].confirmed,
+      'Tasmania': @tas.state_records.order('created_at ASC')[-1].confirmed,
+      'Australian Capital Territory': @act.state_records.order('created_at ASC')[-1].confirmed,
+      'Northern Territory': @nt.state_records.order('created_at ASC')[-1].confirmed,
     }
 
     render json: @overview
